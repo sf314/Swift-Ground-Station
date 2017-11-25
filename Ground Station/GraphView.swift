6ac349c1384425736a28ab9567acb25c8c126d8a
smflore4 <smflore4@asu.edu>
2017-11-25 00:54:16 -0700

    Added GraphView skeleton

---
+++ Ground Station/GraphView.swift
@@ -0,0 +1,53 @@
+//
+//  GraphView.swift
+//  Ground Station
+//
+//  Created by Stephen Flores on 11/23/17.
+//  Copyright © 2017 Stephen Flores. All rights reserved.
+//
+
+/*
+ Subclass of NSView that implements graphing functionality. Will hold n number
+ of data points between max and min. The maximum and minimum values will be
+ displayed on the left margin.
+ */
+
+import Foundation
+import Cocoa
+
+
+class GraphView: NSView {
+    
+    // MARK: - Data variables and manipulation
+    var data: [Double] = []
+    
+    var min = 0.0
+    var max = 0.0
+    var size = 100
+    var debugMode = false
+    
+    func add(_ point: Double) {
+        data.append(point)
+        if data.count > size {
+            data.remove(at: 0)
+        }
+    }
+    
+    // MARK: - Drawing
+    override func draw(_ rect: NSRect) {
+        super.draw(rect)
+    }
+    
+    func updateDisplay() {
+        self.setNeedsDisplay(self.bounds)
+    }
+    
+    // MARK: - Debugging
+    func debug(_ s: String) {
+        if debugMode {
+            print(s)
+        }
+    }
+    
+    
+}
